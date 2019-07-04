efe69667835e95abb2774305ee81a938db9c0bfa
Vasilache Catalin <catalin.vasilache@tremend.com>
2019-07-04 14:18:27 +0300

    Create cell called square

---
+++ snake.js
@@ -0,0 +1,21 @@
+// const rows = 10;
+// const cols = 10;
+// let board = [ ];
+// const free = "#228B22";
+//
+// function createBoard(){
+//     document.querySelector('tb').innerHtml = document.createElement('tr');
+//     //document.getElementsByTagName("table").style.color = "red";
+//
+//     for (let i = 0; i < rows; i++){
+//         board[i] = [ ];
+//         document.querySelector('tr').innerHtml = document.createElement("tr");
+//
+//         for (let j = 0; j < cols; j++) {
+//             board[i][j] = free;
+//             document.getElementsByTagName("tr").innerHtml = document.createElement("td");
+//         }
+//     }
+// }
+// setInterval(createBoard, 1000);
+// //createBoard();
