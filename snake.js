4a18520612ed4ccf9388fc526fb9a82ff1c940c6
Vasilache Catalin <catalin.vasilache@tremend.com>
2019-07-05 17:01:22 +0300

    Color a random cell and then move the cell color according to user's choice of key (unresolved).

--- snake.js
+++ snake.js
@@ -1,6 +1,5 @@
+
 function createBoard(rows, cols){
-
-
     for(let i=0; i<rows; i++) {
         const ro = document.createElement("div");
         ro.id=`${i}`;
@@ -16,4 +15,60 @@ function createBoard(rows, cols){
         }
     }
 }
-createBoard(15,15);
+
+ function getRandomNumber(min, max) {
+     min = Math.ceil(min);
+     max = Math.floor(max);
+     return Math.floor(Math.random() * (max - min)) + min;
+ }
+
+function getRandomPosition (rows, cols){
+    let randomR = getRandomNumber(0, rows);
+    let randomC = getRandomNumber(0, cols);
+    return [randomR, randomC];
+}
+
+ function colorCell([row, column]){
+     const rowElement = document.getElementById(`${row}`);
+     const cell = rowElement.childNodes[column];
+     cell.style.backgroundColor = "red";
+ }
+
+ function keyListener(){
+    window.addEventListener("keypress", motionCell )
+ }
+
+function motionCell(randomRow, randomColumn, direction) {
+    colorCell([randomRow, randomColumn]);
+    switch (direction){
+        case "left":
+            setInterval(() => colorCell([randomRow, randomColumn--]), 100);
+            break;
+        case "right":
+            setInterval(() => colorCell([randomRow, randomColumn++]), 100);
+            break;
+        case "up":
+            setInterval(() => colorCell([randomRow--, randomColumn]), 100);
+            break;
+        case "down":
+            setInterval(() => colorCell([randomRow++, randomColumn]), 100);
+            break;
+        default:
+            setInterval(() => colorCell([randomRow, randomColumn]), 100);
+
+    }
+}
+
+function initGame(){
+    let rows = 20;
+    let cols = 20;
+     createBoard(rows,cols);
+    let [randomRow, randomColumn] = getRandomPosition(rows,cols);
+    motionCell(randomRow, randomColumn, "right");
+}
+
+initGame();
+
+
+
+
